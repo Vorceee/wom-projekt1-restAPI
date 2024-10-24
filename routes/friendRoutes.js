42fb6b22ef58e48483e40c7e582755695fbcec54
Vorce <fellefelixa@gmail.com>
2024-10-25 02:39:34 +0300

    get pending

--- routes/friendRoutes.js
+++ routes/friendRoutes.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const { sendFriendRequest, respondToFriendRequest, getFriends } = require('../controllers/friendController');
+const { sendFriendRequest, respondToFriendRequest, getFriends, getPendingFriendRequests } = require('../controllers/friendController');
 const authenticateToken = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
 router.post('/request', authenticateToken, sendFriendRequest);
-
 router.post('/respond', authenticateToken, respondToFriendRequest);
-
 router.get('/', authenticateToken, getFriends);
+router.get('/pending', authenticateToken, getPendingFriendRequests);  // New route for pending requests
 
 module.exports = router;
